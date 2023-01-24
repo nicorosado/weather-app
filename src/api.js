4ab29493f1d25a6a8a20a4cd0342e18fa5240a31
nicorosado <nicorosado.nr@gmail.com>
2023-01-24 11:53:51 -0300

    Weather app

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApioptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "b01574aa4emsh7c9ea9384f40371p1c41b2jsnada9a7ca2ab1",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "642b8cca71b376a04d11133647e004ef";
